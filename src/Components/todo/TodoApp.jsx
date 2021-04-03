a8369e753e082646a97beea7c7d0492842988621
Niraj Sngh <you@example.com>
2021-04-03 17:01:07 +0530

    conditon messge added for authentication (created function component for that)

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -25,20 +25,26 @@ class LoginComponent extends Component
         this.state={
             usernameplaceholer:'username',
             username:'',
-            password:''
+            password:'',
+            hasLoginFailed:false,
+            showSuccessMessage:false
         }
        // used arrow function so no use of binding
       ///  this.handleUserNameChange=this.handleUserNameChange.bind(this);
         //this.handlePasswordChange=this.handlePasswordChange.bind(this);
             //this.changeEvent=this.changeEvent.bind(this);
+
+            this.loginClicked=this.loginClicked.bind(this);
     }
     render()
     {
         return(
             <div className="LoginComponent">
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
                 Username  <input type="text" name="username" value={this.state.username} onChange={this.changeEvent} placeholder={this.state.usernameplaceholer}/>&nbsp;
                 Password  <input type="password" name="password" value={this.state.password} onChange={this.changeEvent}/>&nbsp;&nbsp;
-                <button type="submit" name="submit">Login</button>
+                <button type="submit" name="submit" onClick={this.loginClicked}>Login</button>
             </div>
 
         );
@@ -58,11 +64,45 @@ class LoginComponent extends Component
 //common change event
 changeEvent=(event)=>
 {
-    console.log(event.target.name);
-    console.log(event.target.value);
+   // console.log(event.target.name);
+    //console.log(event.target.value);
     this.setState({ [event.target.name]:event.target.value })
 }
 
+loginClicked()
+{
+    if(this.state.username==='a' && this.state.username==='a')
+    {
+        this.setState({hasLoginFailed:false,showSuccessMessage:true});
+        console.log("success");
+    }else{
+        this.setState({hasLoginFailed:true,showSuccessMessage:false});
+        console.log("failed");
+    }
+}
+}
 
+
+//function component for auth(failed)
+function ShowInvalidCredentials(props)
+{
+if(props.hasLoginFailed)
+{
+    return   <div>login failed</div>;
+}
+
+return null;
+}
+
+
+//function component for auth(success)
+function ShowLoginSuccessMessage(props)
+{
+if(props.showSuccessMessage)
+{
+    return    <div>Successfull loggedin</div>
+}
+
+return null;
 }
 export default TodoApp;
